8d8dc160c48f8c40d7390a0b4e87257071756ce5
raj0987-alt <71288207+raj0987-alt@users.noreply.github.com>
2023-06-20 10:16:21 +0600

    Get Request

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreWebAPIDBFirst.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebAPIDBFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public readonly employeedbContext _context;
+        public EmployeesController(employeedbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployees()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+    }
+}
